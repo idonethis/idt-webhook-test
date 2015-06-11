a5177098793999d7ee03d866b49d0a36a7bfd36c
Francesco Levorato <git@flevour.net>
2015-06-11 11:25:01 +0200

    add request logging and support for POST

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, abort
+from flask import Flask, abort, request
 import random
 import os
 
@@ -6,30 +6,41 @@ import os
 app = Flask(__name__)
 
 
+@app.before_request
+def log_request():
+    print "=== HEADERS ==="
+    print request.headers
+    print "=== VALUES ==="
+    print request.values
+    print "=== BODY ==="
+    print request.data
+
+
 @app.route('/')
 def index():
     return "<pre>{}</pre>".format(str(app.url_map).replace('<', '&lt;'))
 
 
-@app.route('/ok')
+@app.route('/ok', methods=['GET', 'POST'])
 def ok():
     return "ok"
 
 
-@app.route('/fail')
-@app.route('/fail/<int:code>')
+@app.route('/fail', methods=['GET', 'POST'])
+@app.route('/fail/<int:code>', methods=['GET', 'POST'])
 def fail(code=None):
     codes = [400, 401, 404, 500]
     code = code if code and code in codes else random.choice(codes)
     abort(code)
 
 
-@app.route('/random')
+@app.route('/random', methods=['GET', 'POST'])
 def random_response():
     if random.random() > 0.9:
         return random()
     else:
         return ok()
+
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
